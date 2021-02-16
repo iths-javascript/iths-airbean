716427f00e736fcaa7e9525420b08ae957870a65
codeninjah <codeninjahblog@gmail.com>
2021-02-16 18:52:51 +0100

    progress

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,15 +1,46 @@
 <template>
-    <h1>profilepage</h1>
+<div>
+    <h1 id="h1" v-if="visible" v-on:click="LoggIn">Profilepage Ska visas EN gång. Klick på DEN!</h1>
+    <h1 id="header1" v-if="notvisible">Profilepage. Ska visas efter att man loggat in</h1>
+    </div>
 </template>
 
 
 <script>
 export default {
-    name: "ProfilePage"
+    data() { return {
+        visible: true,
+        notvisible : false
+    }},
+    name: "ProfilePage",
+
+
+    methods: {
+        LoggIn(){
+            localStorage.setItem("inloggad", "true")
+
+            const x = document.getElementById("h1")
+            x.style.display = "none"          
+            
+            const y = document.getElementById("header1")
+            y.style.display = "block"
+        }
+    },
+
+    created(){
+        //const y = document.getElementById("header1")
+
+        if(localStorage.getItem("inloggad")){     
+            this.visible = false
+            this.notvisible = true
+            //y.style.display = "block"
+        }
+    }
 }
 </script>
 
 
-<style>
+<style scoped>
+
 
 </style>
