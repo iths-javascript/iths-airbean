25102c82ff12b855c7a3d052776043e77071efd2
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-02-17 11:05:28 +0100

    Complete Landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+<router-link to="/about">
+  <section class="home">
+      <img src="@/assets/airbean-landing.svg" alt="">
+  </section>
+</router-link>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  methods: {},
-  created() {},
+  name: "Home"
 };
 </script>
+
+<style lang="scss" scoped>
+.home{
+  height: 100%;
+  background: 
+    url('../assets/intro-graphic-left.svg')
+    top left
+    no-repeat,
+    url('../assets/intro-graphic-right.svg')
+    top right
+    no-repeat
+  ;
+  background-color: $airbean-green;
+  background-size: contain;
+
+  display: grid;
+  place-items: center;
+}
+</style>
