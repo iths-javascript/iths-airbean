f9de3af98b60650826f29a1739ababfbacddbe5d
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-02-17 13:44:32 +0100

    Move styles to assets folder

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     css: {
       loaderOptions: {
         sass: {
-          prependData: `@import "@/styles/_colors.scss";`
+          prependData: `@import "@/assets/styles/_colors.scss";`
         }
       }
     }
