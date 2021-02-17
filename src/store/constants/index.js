86b754985468bd16e3e13e72bc983739390e7bb8
richard-iths <richard.forsberg@iths.se>
2021-02-17 11:18:39 +0100

    added actions/mutations to login,logout and register a user and getters to get user andis logged in state and added constants module for mutations

---
+++ src/store/constants/index.js
@@ -0,0 +1,5 @@
+export const userConstants = {
+  SET_LOGIN: "setLogin",
+  SET_LOGOUT: "setLogout",
+  SET_USER: "setUser",
+};
--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,12 +1,50 @@
+import { userConstants as Mutations } from "@/store/constants";
+import * as API from "@/api/mock";
 const user = {
   namespaced: true,
   state: () => ({
     isLoggedIn: false,
-    userId: "",
+    user: {},
   }),
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters: {
+    getUser(state) {
+      return state.user;
+    },
+    isLoggedIn(state) {
+      return state.isLoggedIn;
+    },
+  },
+  mutations: {
+    [Mutations.SET_LOGIN](state, user) {
+      state.user = user;
+      state.isLoggedIn = true;
+    },
+    [Mutations.SET_LOGOUT](state) {
+      state.user = {};
+      state.isLoggedIn = false;
+    },
+  },
+  actions: {
+    async setLogin({ commit, email }) {
+      try {
+        const user = await API.loginUser(email);
+        commit(Mutations.SET_LOGIN, user);
+      } catch (error) {
+        return error.message;
+      }
+    },
+    async setLogout({ commit }) {
+      commit(Mutations.SET_LOGOUT);
+    },
+    async registerUser({ commit }, userData) {
+      try {
+        const user = await API.registerUser(userData.name, userData.email);
+        commit(Mutations.SET_LOGIN, user);
+      } catch (error) {
+        return error.message;
+      }
+    },
+  },
 };
 
 export default user;
