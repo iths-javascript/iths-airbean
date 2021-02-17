2fcb855f3d09052ba16fe0e552a131a3ec32b302
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-02-17 21:45:21 +0100

    Add about page with content and placeholder styling

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ export default {
 }
 
 #app {
-  height: 100vh;
+  min-height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
