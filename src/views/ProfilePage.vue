62b9b148d6ab9330c461ffedb6b1e5311eabe790
sohailbhatti2015 <bhatti_bkk@hotmail.com>
2021-02-18 14:41:37 +0100

    test

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,9 +1,14 @@
 <template>
 <div>
-    <h1 id="h1" v-if="visible" v-on:click="LoggIn">
-        <router-link to="/">Profilepage Ska visas EN gång. Klick på DEN!</router-link></h1>
+    <router-link to="/menu">
+    <button v-if="visible" v-on:click="LoggIn">
+       Brew me a cup 
+    </button>
+    </router-link>
     <h1 id="header1" v-if="notvisible">Profilepage. Ska visas efter att man loggat in</h1>
+    
     </div>
+    
 </template>
 
 
@@ -20,7 +25,7 @@ export default {
         LoggIn(){
             localStorage.setItem("inloggad", "true")
 
-            const x = document.getElementById("h1")
+            const x = document.getElementsByTagName("button")[0]
             x.style.display = "none"          
             
             const y = document.getElementById("header1")
@@ -42,9 +47,18 @@ export default {
 
 
 <style scoped>
+button{
+position: center;
+width: 248px;
+height: 55px;
+/* left: 64px;
+top: 450px; */
+color:white;
+font-size: 22px;
 
-router-link {
-    text-decoration: none;
+/* Airbean - brown */
+
+background: #2F2926;
+border-radius: 50px;
 }
-
 </style>
